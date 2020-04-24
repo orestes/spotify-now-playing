1a7ec01528d9a0f60348677ca8acb250f70196d4
Orestes Carracedo <orestes@orestes.io>
2020-04-24 21:13:01 +0200

    Extract firebase config to a separate file and ignore firebase settings and cache

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,7 @@
 import {Config} from '../config';
+import firebase from '../../firebase-credentials';
 
 export const environment: Config = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyA8QX_uGwN9mOt_XVDljtqLMrpNHJo47kg',
-    authDomain: 'spotify-watcher-185e9.firebaseapp.com',
-    databaseURL: 'https://spotify-watcher-185e9.firebaseio.com',
-    projectId: 'spotify-watcher-185e9',
-    storageBucket: 'spotify-watcher-185e9.appspot.com',
-    messagingSenderId: '230581828104',
-    appId: '1:230581828104:web:ab83862d48b29f1ad06fbd'
-  },
+  firebase,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,19 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 import {Config} from '../config';
+import firebase from '../../firebase-credentials';
 
 export const environment: Config = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyA8QX_uGwN9mOt_XVDljtqLMrpNHJo47kg',
-    authDomain: 'spotify-watcher-185e9.firebaseapp.com',
-    databaseURL: 'https://spotify-watcher-185e9.firebaseio.com',
-    projectId: 'spotify-watcher-185e9',
-    storageBucket: 'spotify-watcher-185e9.appspot.com',
-    messagingSenderId: '230581828104',
-    appId: '1:230581828104:web:ab83862d48b29f1ad06fbd'
-  },
+  firebase,
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
